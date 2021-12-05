eb0005b70fb2522b78b29874dc44dff03e2125ec
Leo Orpilla III <dev@ldgrp.me>
2021-12-05 17:47:42 +1000

    Add day 5 solution

--- README.md
+++ README.md
@@ -9,20 +9,23 @@ _Domino from Amphibia S03E09_
 
 ## Code
 
-| Day | Prompt             | Solution            |
+| Day | Prompt             | Solution        |
 | --- | ------------------ | --------------- |
 | 1   | [prompt][prompt01] | [src][day01src] |
 | 2   | [prompt][prompt02] | [src][day02src] |
 | 3   | [prompt][prompt03] | [src][day03src] |
 | 4   | [prompt][prompt04] | [src][day04src] |
+| 5   | [prompt][prompt05] | [src][day05src] |
 
 [day01src]: src/day01.hs
 [day02src]: src/day02.hs
 [day03src]: src/day03.hs
 [day04src]: src/day04.hs
+[day05src]: src/day05.hs
 
 [aoc]: https://adventofcode.com/2021
 [prompt01]: https://adventofcode.com/2021/day/1
 [prompt02]: https://adventofcode.com/2021/day/2
 [prompt03]: https://adventofcode.com/2021/day/3
 [prompt04]: https://adventofcode.com/2021/day/4
+[prompt05]: https://adventofcode.com/2021/day/5
---
+++ src/day05.hs
@@ -0,0 +1,28 @@
+import Data.List.Split
+import qualified Data.Map as M
+
+type Point = (Int, Int)
+type Line = (Point, Point)
+type Board = M.Map Point Int
+
+parseLine :: String -> Line
+parseLine s = ((x1, y1), (x2, y2))
+    where
+        ((x1:y1:_):(x2:y2:_):_) = (fmap read) <$> splitOn "," <$> splitOn "->" s
+
+drawLine :: Line -> Board -> Board
+drawLine ((x1, y1), (x2, y2)) board = foldr drawPoint board [(x1 + dx * i, y1 + dy * i) | i <- [0..d]]
+    where d = max (abs (x1 - x2)) (abs (y1 - y2))
+          dx = signum (x2 - x1)
+          dy = signum (y2 - y1)
+
+drawPoint :: Point -> Board -> Board
+drawPoint = flip (M.insertWith (const (+ 1))) 1
+
+isDiagonal :: Line -> Bool
+isDiagonal ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
+
+main = do
+    vents <- fmap parseLine <$> lines <$> readFile "input05.txt"
+    print $ length $ M.filter (>= 2) $ foldr drawLine M.empty $ filter isDiagonal vents
+    print $ length $ M.filter (>= 2) $ foldr drawLine M.empty vents
